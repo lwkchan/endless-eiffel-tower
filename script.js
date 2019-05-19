a298b777dd6d498c8580497b08461c6d9f06a947
lwkchan <laurachan205@gmail.com>
2019-05-19 11:13:34 +0100

    connects styles

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 ;(function(document) {
-  // all your code here
   let foo = function() {
     let el = document.getElementById('main')
     el.innerText = 'Now you are different'
   }
   window.onload = foo
-  // ...
 })(document)
